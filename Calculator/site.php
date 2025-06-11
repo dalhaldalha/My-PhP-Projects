301798768f8d29f5cb2eb1887e66137e742eabe2
Dalha <dalhalawand@gmail.com>
2025-06-12 00:34:21 +0100

    Starting Over

--- Calculator/site.php
+++ Calculator/site.php
@@ -17,62 +17,8 @@
 // Extras
 // 1. User should be able to select a theme (dark mode).
 -->
-    <div>
-        
-    </div>
-    <?php
-        $num1 = $_POST['num1'] ?? null;
-        $num2 = $_POST['num2'] ?? null;
-        $operator = $_POST['operator'] ?? null;
-        $result = null;
-        
-        if ($num1 !== null && $num2 !== null && $operator !== null) {
-            switch ($operator) {
-                case '+':
-                    $result = $num1 + $num2;
-                    break;
-                case '-':
-                    $result = $num1 - $num2;
-                    break;
-                case '*':
-                    $result = $num1 * $num2;
-                    break;
-                case '/':
-                    $result = $num1 / $num2;
-                    break;
-                default:
-                    $result = "Invalid operator";
-                    break;
-                }         
-            }
-    ?>
     <h1>Calculator</h1>
-    <form action="site.php" method="post" >
-        <!-- <input type="number" name="num1" required>
-        <input type="number" name="num2" required>
-    
-        <button type="submit" name="operator" value="+">+</button>
-        <button type="submit" name="operator" value="-">-</button>
-        <button type="submit" name="operator" value="*">x</button>
-        <button type="submit" name="operator" value="/">/</button> -->
-        
-        <!-- User inputs  -->
-        <button type="submit" value="1" name="num1">1</button>
-        <button type="submit" value="2" name="num2">2</button>
-        <button type="submit" value="+" name="operator">+</button>
-        <button type="submit" value="-" name="operator">-</button>
-        <button type="submit" value="*" name="operator">x</button>
-        <button type="submit" value="/" name="operator">/</button>
 
-
-        <!-- Inputs Coverted to Hidden -->
-        <?php if ($num1 !== null || $num2 !== null) : ?>
-            <input type="hidden" name="num1" value="<?php echo $num1; ?>">
-            <input type="hidden" name="num2" value="<?php echo $num2; ?>">
-        <?php endif; ?>
-
-    </form>
-        
 </body>
 
 </html>
