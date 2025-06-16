bf5bd8f1ee38f6a38e7ac76b5d86c1ee80c76e82
Dalha <dalhalawand@gmail.com>
2025-06-16 23:54:24 +0100

    Commit

--- BMI Calculator/index.php
+++ BMI Calculator/index.php
@@ -8,13 +8,24 @@
 <body>
     Calculate your BMI:
     <?php 
-    $weight = $_Post['weight'] ?? '';
-    $height = $_Post['height'] ?? '';
+    // How it should work:
+    // 1. User Should be able to enter their weight and height.
+    // 2. Program should be able to calculate BMI using the two inputs.
+    // 3. Program Displays the BMI Value and tell you what class you fall into.
+    // 4. Program should display all categories of BMI. together with repective colors. 
+
+        $weight = $_Post['weight'] ?? '';
+        $height = $_Post['height'] ?? '';
+
+        echo $_Post['weight'] ?? 'Weight not set';
     ?>
 
     <form action="site.php" method='post'>
         <label for="weight">Weight (kg):</label>
-        
+        <input type="number" name="weight" placeholder="Enter your weight" required>
+        <br><br>
+        <label for="height">Height (m):</label>
+        <input type="number" name="height" placeholder="Enter your height" required>
 
     </form>
 </body>
