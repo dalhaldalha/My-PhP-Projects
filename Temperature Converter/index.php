8bb67bf71fe572026537d02512ea8d946396197c
Dalha <dalhalawand@gmail.com>
2025-06-20 22:34:30 +0100

    commit

--- Temperature Converter/index.php
+++ Temperature Converter/index.php
@@ -23,16 +23,16 @@
             <input type="number" name="value" step="0.01"> <br>
             <label for="from">From: </label> <br>
             <select name="from" id="from">
-                <option value="Celsius">Celsius</option>
-                <option value="Fahrenheit">Fahrenheit</option>
-                <option value="Kelvin">Kelvin</option>
+                <option value="C">Celsius</option>
+                <option value="F">Fahrenheit</option>
+                <option value="K">Kelvin</option>
             </select> <br>
                 
             <label for="to">To: </label> 
             <select name="to" id="">
-                <option value="Fahrenheit">Fahrenheit</option>
-                <option value="Celsius">Celsius</option>
-                <option value="Kelvin">Kelvin</option>
+                <option value="F">Fahrenheit</option>
+                <option value="C">Celsius</option>
+                <option value="K">Kelvin</option>
             </select> <br>
 
             <button type="submit">Calculate</button>
@@ -41,11 +41,41 @@
 
     <?php 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // 1. Get the input value
-            $value = $_POST["value"];
-            // 2. Get the "from" and "to" values
-            $from = $_POST["from"];
-            $to = $_POST["to"];
+            // // 1. Get the input value
+            // 
+            // // 2. Get the "from" and "to" values
+            // 
+
+            function converTemperature($value, $from, $to) {
+                $value = $_POST["value"];
+                $from = $_POST["from"];
+                $to = $_POST["to"];
+
+                if ($from == "C") {
+                    switch ($to) {
+                        case "C":
+                            echo "$value °C is equal to $value °C";
+                            break;
+                        case "F":
+                            echo "$value °C is equal to " . (($value*9/5)+32) . " °F";
+                            break;
+                        case "K":
+                            echo "$value °C is equal to " . ($value +273.15) . " K";
+                            break;
+                        default:
+                            echo "Invalid temperature scale selected.";
+                            break;
+                    }
+                } else if ($from == "F") {
+
+                } else if ($from == "K") {
+
+                } else {
+                    echo "Invalid temperature scale selected.";
+                }
+
+
+            }
         }
     ?>
 
