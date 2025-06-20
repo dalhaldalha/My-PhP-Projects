6f3b9c7cdf326bf1b7daa7b0a218242ffae9128c
Dalha <dalhalawand@gmail.com>
2025-06-20 02:11:08 +0100

    commit

--- Functions Practice/index.php
+++ Functions Practice/index.php
@@ -1,3 +1,6 @@
+<?php 
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,33 +11,15 @@
 <body>
     
     <?php 
-        session_start();
-
         echo "Welcome to the random number generator" . "<br>"
             . "Guess a number between 0 and 100" . "<br>";
 
         if (!isset($_SESSION["randomNumber"])) {
-            $_SESSION["randomNumber"] = rand(0, 100);
+            $_SESSION["randomNumber"] = floatval(rand(0, 100));
         } 
 
+
         
-
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $guess = htmlspecialchars($_POST["guess"]);
-            $randomNumber = $_SESSION["randomNumber"];
-
-            if ($guess == $randomNumber) {
-                echo "Congratulations! You guessed the number: $randomNumber";
-                unset($_SESSION["randomNumber"]); // Reset the game
-            } else if ($guess < $randomNumber) {
-                echo "Your guess is too low.";
-            } else if ($guess > $randomNumber) {
-                echo "Your guess is too high.";
-            } else {
-                echo "Your guess is invalid.";
-            }
-
-        }
     ?>
     
     <form action="" method="post">
@@ -44,6 +29,25 @@
 
     </form>
 
+    <?php 
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $guess = htmlspecialchars($_POST["guess"]);
+            $randomNumber = $_SESSION["randomNumber"];
+
+            if ($guess == $randomNumber) {
+                echo "Congratulations! You guessed the number: $randomNumber" . "<br>";
+                unset($_SESSION["randomNumber"]); // Reset the game
+            } else if ($guess < $randomNumber) {
+                echo "Your guess is too low." . "<br>";
+            } else if ($guess > $randomNumber) {
+                echo "Your guess is too high." . "<br>";
+            } else {
+                echo "Your guess is invalid.";
+            }
+            
+            print_r($_SESSION) .  "<br>"; 
+        }
+    ?>
     
 </body>
 </html>
