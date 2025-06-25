a4c4c9b168f0742768a7805b1b0f730f3270aca3
Dalha <dalhalawand@gmail.com>
2025-06-25 04:11:06 +0100

    commit

---
+++ Trivana/categories/general.cat.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/general.css">
+    <title>General Knowledge</title>
+</head>
+<body>
+    <section class="container">
+        <h2>General Knowledge</h2>
+        <hr class="divider">
+        <p>What is the capital of Australia?</p>
+        <div class="options-container">
+            <div class="option_1 options">option 1</div>
+            <div class="option_2 options">option 2</div>
+            <div class="option_3 options">option 3</div>
+            <div class="option_4 options">option 4</div>
+        </div>
+
+    </section>
+</body>
+</html>
