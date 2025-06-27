6ba967c99e811e54645dbfbb5f3fd98b00bae144
Dalha <dalhalawand@gmail.com>
2025-06-27 03:05:21 +0100

    commit

--- Trivana/categories/general.cat.php
+++ Trivana/categories/general.cat.php
@@ -6,21 +6,18 @@ session_start(); //Starts session
 
         //First Query
         $query = "SELECT name FROM categories WHERE name = :category;"; // Does a SQL query to SELECT a category inside the database categories.
-
         $stmt = $pdo->prepare($query); //This line is responsible for sumbiting the query into the database.
-
         $stmt->bindParam(":category", $_SESSION["category"]); //Binds the user category to the database "categories
         $stmt->execute(); // This line Final excutes the Query.
-
         $results = $stmt->fetch(PDO::FETCH_ASSOC);
 
         //Second Query
         $query2 = "SELECT * FROM questions;";
         $stmt2 = $pdo->prepare($query2);
         $stmt2->execute();
-
         $results2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
 
+        //Resets all values to null
         $pdo = null;
         $stmt = null;
 
@@ -48,23 +45,27 @@ session_start(); //Starts session
         <h2 class="heading-txt"><?php echo htmlspecialchars($results["name"]) ?></h2>
         <p class="question-p"><?php echo $results2[0]["question_text"]; ?></p>
         <div class="options-container">
-            <button class="option_1 options"><?php echo $results2[0]["option_a"]; ?></button>
-            <button class="option_2 options"><?php echo $results2[0]["option_b"]; ?></button>
-            <button class="option_3 options"><?php echo $results2[0]["option_c"]; ?></button>
-            <button class="option_4 options"><?php echo $results2[0]["option_d"]; ?></button>
+            <button class="option_1 options"><?php echo $results2[0]["A"]; ?></button>
+            <button class="option_2 options"><?php echo $results2[0]["B"]; ?></button>
+            <button class="option_3 options"><?php echo $results2[0]["C"]; ?></button>
+            <button class="option_4 options"><?php echo $results2[0]["D"]; ?></button>
         </div>
 
     </section>
 
     <?php 
 
-        // foreach ($results2 as $row) {
-        //     echo htmlspecialchars($row["question_text"]) . "<br>";
-        //     echo htmlspecialchars($row["option_a"]) . "<br>";
-        //     echo htmlspecialchars($row["option_b"]) . "<br>";
-        //     echo htmlspecialchars($row["option_c"]) . "<br>";
-        //     echo htmlspecialchars($row["option_d"]) . "<br> <br>";
-        // }
+        foreach ($results2 as $row) {
+            $correctLetter = $row["correct_option"];
+            $correctAnswer; 
+            echo htmlspecialchars($row["question_text"]) . "<br>";
+            echo htmlspecialchars($row["A"]) . "<br>";
+            echo htmlspecialchars($row["B"]) . "<br>";
+            echo htmlspecialchars($row["B"]) . "<br>";
+            echo htmlspecialchars($row["D"]) . "<br> <br>";
+            echo htmlspecialchars($row["correct_option"]);
+            break;
+        }
     ?>
 </body>
 </html>
