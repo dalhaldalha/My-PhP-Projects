01c68e813b0ad4f34ee2f210cb0ea7ab6f42e061
Dalha <dalhalawand@gmail.com>
2025-06-27 01:46:42 +0100

    commit

--- Trivana/categories/general.cat.php
+++ Trivana/categories/general.cat.php
@@ -1,25 +1,33 @@
 <?php 
-session_start();
+session_start(); //Starts session
 
+    try { 
+        require_once "../includes/dbh.inc.php"; // Links to the codes in the database connection page.
 
-    try {
-        require_once "../includes/dbh.inc.php";
+        //First Query
+        $query = "SELECT name FROM categories WHERE name = :category;"; // Does a SQL query to SELECT a category inside the database categories.
 
-        $category = $_SESSION["category"];
+        $stmt = $pdo->prepare($query); //This line is responsible for sumbiting the query into the database.
 
-        $query = "SELECT name FROM categories WHERE name = :category;";
-
-        $stmt = $pdo->prepare($query);
-
-        $stmt->bindParam(":category", $category);
-        $stmt->execute();
+        $stmt->bindParam(":category", $_SESSION["category"]); //Binds the user category to the database "categories
+        $stmt->execute(); // This line Final excutes the Query.
 
         $results = $stmt->fetch(PDO::FETCH_ASSOC);
 
+        //Second Query
+        $query2 = "SELECT * FROM questions;";
+        $stmt2 = $pdo->prepare($query2);
+        $stmt2->excute();
+
+        $results2 = $stmt2->fetch(PDO::FETCH_ASSOC);
+
         $pdo = null;
         $stmt = null;
+
+        
+
     } catch (PDOException $e) {
-        die("Connection failed: " . $e->getMessage());
+        die("Connection failed: " . $e->getMessage()); // 
     }
 
 ?>
@@ -34,8 +42,9 @@ session_start();
     <title>General Knowledge</title>
 </head>
 <body>
+    <p>Hi, <?php echo isset($_SESSION["username"]) ? htmlspecialchars($_SESSION["username"]) : "User"; ?></p> <br>
     <section class="container">
-        <h2 class="heading-txt"><?php echo htmlspecialchars($results["name"]); ?></h2>
+        <h2 class="heading-txt"><?php echo htmlspecialchars($results["name"]) ?></h2>
         <p class="question-p">What is the capital of Australia?</p>
         <div class="options-container">
             <button class="option_1 options">option 1</button>
--- Trivana/includes/formhandler.inc.php
+++ Trivana/includes/formhandler.inc.php
@@ -9,16 +9,17 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") { // Will only run this code if user a
         require_once "dbh.inc.php"; //links the codes from another file. In this context, it links to our database connection file.
 
         $query = "INSERT INTO users (username) 
-            VALUES (:username);";
+            VALUES (:username);"; //Does a SQL query to insert into the users table, the value of "username"
 
-        $stmt = $pdo->prepare($query);
+        $stmt = $pdo->prepare($query); //This line is responsible for sumbiting the query into the database.
 
-        $stmt->bindParam(":username", $username);
-        $stmt->execute();
+        $stmt->bindParam(":username", $username); //This line binds the data being sumbitted into the database.
+        $stmt->execute(); // This is the final line that excutes the query.
 
-        $pdo = null;
+        $pdo = null; //This two statements cancel out the connection to the database. They are not nessecery because it is done automatically but it is best practive to free up resources as early as possible.
         $stmt = null;
 
+        // The following switch statements checks what the user set category as. Depending on the category value, the user will be redirected to its respective page.
         switch ($category) {
             case "general knowledge":
                 header("Location: ../categories/general.cat.php");
@@ -37,11 +38,11 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") { // Will only run this code if user a
                 break;
         }
 
-        die();
-    } catch (PDOException $e) {
-        die("Connection failed: " . $e->getMessage());
+        die(); //Used for closing off a script that has a connection. Use "exit()" if the script has no connection.
+    } catch (PDOException $e) { //Catches the exception and names it variable $e
+        die("Connection failed: " . $e->getMessage()); //Sends a message if connection to the database is unsuccessful and gives a message as to the reason.
     }
 
-} else {
-    header("Location: ../index.php");
+} else { //If user does not access this page through the "post" method, they will be redirected to the home page.
+    header("Location: ../index.php"); 
 }
