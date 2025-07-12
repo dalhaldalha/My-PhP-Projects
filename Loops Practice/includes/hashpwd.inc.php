befa356d179bc1040c9e14fc7c30bc2289e3daf1
Dalha <dalhalawand@gmail.com>
2025-07-12 11:48:24 +0100

    commit

--- Loops Practice/includes/hashpwd.inc.php
+++ Loops Practice/includes/hashpwd.inc.php
@@ -5,8 +5,27 @@ $userName = "Dalha";
 $salt = bin2hex(random_bytes(16));
 $pepper ="ANotSoSecretPepperString";
 
-$dataToHash = $username . $salt . $pepper;
+$dataToHash = $userName . $salt . $pepper;
 
 $hash = hash("sha256", $dataToHash);
 
-echo "<br>" . $hash;
+/*----------*/
+
+$storedSalt = $salt;
+$storedHash = $hash;
+$pepper = "ANotSoSecretPepperString";
+
+$dataToHash = $userName . $storedSalt . $pepper;
+
+$verificationHash = hash("sha256", $dataToHash);
+
+if ($storedHash === $verificationHash) {
+    echo "The data are the same!" . "<br>";
+    echo "The stored Hash is: " . $storedHash . "<br>";
+    echo "The Verification Hash is: " . $verificationHash . "<br>";
+
+} else {
+    echo "The data are NOT the same!" . "<br>";
+    echo "The stored Hash is: " . $storedHash . "<br>";
+    echo "The Verification Hash is: " . $verificationHash . "<br>";
+}
