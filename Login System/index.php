347f81c70eaffd04adb2081df0cb8e07e4c08d5b
Dalha <dalhalawand@gmail.com>
2025-07-12 17:48:51 +0100

    commit

--- Login System/index.php
+++ Login System/index.php
@@ -1,5 +1,15 @@
 <?php
-
+// Algorithm for Login System
+// 1.Main Page schould give you the option for If your want to Login or sign Up.
+//   a.Login button should take you to a login page.
+//   b. Sign Up button should take you to a sign up page.
+// 2. User should be able to input their personal details to sign up.
+//   a.Make sure that username, email and passowrd are required.
+//   b.All details should be stored in the database.
+//   c.Passwords should be hashed once created before storing in the database.
+//   d.Once all fields are created, user should be taken back to Login Page.
+// 3. User should be able to login with either a suername or an email, with a password.
+//   a.If all details match, User should be taken to a welcome screeen.
 require_once 'config.php';
 
 ?>
@@ -12,6 +22,14 @@ require_once 'config.php';
     <title>Document</title>
 </head>
 <body>
-    
+    <h2>Welcome to Penprofile</h2>
+    <p>How would you like to proceed?</p>
+
+    <form action="inludes/formhandler.inc.php" method="post">
+        <button type="sumbit" name="login">Login</button>
+        <button type="submut" name="signUp">Sign Up</button>
+    </form>
+
+
 </body>
 </html>
