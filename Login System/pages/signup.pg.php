39d3e63db543bdc8c6fa061587211d5e2ecedd6f
Dalha <dalhalawand@gmail.com>
2025-07-14 01:08:16 +0100

    commit

---
+++ Login System/pages/signup.pg.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+    <h2>Sign Up</h2>
+    <p>Tell us about yourself</p>
+    <form action="" method="post">
+        <input type="text" name="username" id="">
+        <input type="email" name="email">
+        <input type="text" name="firstName">
+        <input type="text" name="lastName">
+        <input type="password" name="pwd">
+    </form>
+
+</body>
+</html>
