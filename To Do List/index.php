ccb66e29b9d9cb2f1d386bfcb485f65ba95d67ad
Dalha <dalhalawand@gmail.com>
2025-09-03 14:40:15 +0100

    commit

--- To Do List/index.php
+++ To Do List/index.php
@@ -34,51 +34,53 @@
 </head>
 <body>
     <h1>To Do List</h1>
-    <form action="includes/addTask.php" method="post">
-        <input type="text" name="newTask" placeholder="Write a new task" required>
-        
-        <button type="submit">Add Task</button>
-    </form>
+    <div class="task-div">
+        <form class="add-task" action="includes/addTask.php" method="post">
+            <input class="text-area" type="text" name="newTask" placeholder="Write a new task" required>
+            
+            <button class="add-task-btn" type="submit">&#43;</button>
+        </form>
 
-    <form action="" method="post">
-        <input type="text" name="searchTerm">
-        <button type="submit" >Search</button>
-    </form>
+        <!-- <form action="" method="post">
+            <input type="text" name="searchTerm">
+            <button type="submit" >Search</button>
+        </form> -->
 
-    <h2>Search Results</h2>
-    <?php
-        require_once "includes/searchTask.php";
+        <!-- <h2>Search Results</h2> -->
+        <!-- <?php
+            // require_once "includes/searchTask.php";
 
-        if (empty($results)) {
-            echo "No Results";
-        } else {
+            // if (empty($results)) {
+            //     echo "No Results";
+            // } else {
 
-            var_dump($results);
-            // foreach ($results as $result) {
-            //     echo htmlspecialchars($result["task"] . "<br>");
+            //     var_dump($results);
+            //     // foreach ($results as $result) {
+            //     //     echo htmlspecialchars($result["task"] . "<br>");
+            //     // }
             // }
-        }
-    ?>
+        ?> -->
 
-    <div>
-        <h2>All Tasks</h2>
-        <div class="form-div">
-            <?php foreach ($tasks as $task): ?>
-                <form class="form" action="includes/deleteTask.php" method="post">
-                    <input type="checkbox" name="" id="">
-                    <label><?php echo htmlspecialchars($task['task']); ?></label>
-                    <!-- <select name="priority" id="">
-                        <option value="">High</option>
-                        <option value="">Medium</option>
-                        <option value="">Low</option>
-                    </select> -->
-                    
-                    <input type="hidden" name="task_id" value="<?php echo $task['id']; ?>">
-                    <button class="delete-btn" type="submit" name="delete"> <img class="trash-can-icon" src="assets/trash-can.png" alt="Trash-Can"> </button>
-                </form>
-            <?php endforeach; ?>
+        <div class="all-task-div, task-list">
+            <h2>All Tasks</h2>
+            <div class="form-di">
+                <?php foreach ($tasks as $task): ?>
+                    <form class="each-task" action="includes/deleteTask.php" method="post">
+                        <input class="checkbox" type="checkbox" name="" id="">
+                        <label class="task-content"><?php echo htmlspecialchars($task['task']); ?></label>
+                        <!-- <select name="priority" id="">
+                            <option value="">High</option>
+                            <option value="">Medium</option>
+                            <option value="">Low</option>
+                        </select> -->
+                        
+                        <input type="hidden" name="task_id" value="<?php echo $task['id']; ?>">
+                        <button class="delete-btn" type="submit" name="delete"> <img class="trash-can-icon" src="assets/trash-can.png" alt="Trash-Can"> </button>
+                    </form>
+                <?php endforeach; ?>
+            </div>
+            
         </div>
-        
     </div>
 
     <script>
