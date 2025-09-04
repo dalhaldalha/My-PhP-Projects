b1530f1b2be3ec70d022dbf9d26264f7db7d7ee9
Dalha <dalhalawand@gmail.com>
2025-09-05 00:04:48 +0100

    commit

--- To Do List/includes/exportTask.php
+++ To Do List/includes/exportTask.php
@@ -1,8 +1,13 @@
 <?php
 
-$taskfile = fopen("mytasks.txt", "w");
-$dummytext = "This is a dummy text to test file writing.";
-fwrite($taskfile, $dummytext);
-fclose($taskfile);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $taskfile = fopen("mytasks.txt", "w");
+    $dummytext = "This is a dummy text to test file writing.";
+    fwrite($taskfile, $dummytext);
+    fclose($taskfile);
+} else {
+    header("Location: ../index.php");
+    exit();
+}
 
-echo $taskfile;
+
