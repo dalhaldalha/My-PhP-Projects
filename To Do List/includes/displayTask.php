81f0fa9dc7b4aa2da504bd3eb535dc6762ef4cbf
Dalha <dalhalawand@gmail.com>
2025-09-06 23:18:20 +0100

    commit

--- To Do List/includes/displayTask.php
+++ To Do List/includes/displayTask.php
@@ -2,8 +2,16 @@
 
 require_once "config/database.php";
 
-$query = "SELECT * FROM tasks ORDER BY created_at DESC;";
+$query = "SELECT * FROM tasks ORDER BY created_at DESC LIMIT 4;";
 $stmt = $pdo->prepare($query);
 $stmt->execute();
 $tasks = $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+if (mysqli_num_rows($tasks) > 0) {
+    // while () {
+
+    // }
+} else {
+    echo "You have no tasks";
+}
 
