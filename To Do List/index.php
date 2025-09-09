500fb5451007389d51e87dafa41cb960295b5342
Dalha <dalhalawand@gmail.com>
2025-09-09 21:24:00 +0100

    commit

--- To Do List/index.php
+++ To Do List/index.php
@@ -76,19 +76,28 @@
                     <?php
                     if ($numRows > 0) {
                         foreach ($tasks as $task) {
-                            echo "<div class='end-to-end'>";
-                                echo "<input class='checkbox' type='checkbox'>";
-                                echo "<p class='task-content'>";
-                                    echo $task["task"];
-                                echo "</p>";
-                            echo "</div>";
-                            echo "<div class='end-to-end'>";
-                                echo "<select>";
-                                    echo "<option class='status-incomplete'>Incomplete</option>";
-                                    echo "<option class='status-done'>Done</option>";
-                                echo "</select>";
+                            echo "<div class='each-task'>";
+                                echo "<div class='end-to-end'>";
+                                    echo "<input class='checkbox' type='checkbox'>";
+                                    echo "<p class='task-content'>";
+                                        echo $task["task"];
+                                    echo "</p>";
+                                echo "</div>";
+                                echo "<div class='end-to-end'>";
+                                    echo "<select>";
+                                        echo "<option class='status-incomplete'>Incomplete</option>";
+                                        echo "<option class='status-done'>Done</option>";
+                                    echo "</select>";
+                                    echo "<button class='delete-btn' type='submit' name='delete'>";
+                                        echo "<svg class='trash-can-icon'  xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>";
+                                            echo "<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z'/>";
+                                            echo "<path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z'/>";
+                                        echo "</svg>";
+                                    echo "</button>";
 
+                                echo "</div>";
                             echo "</div>";
+                            
                             
                         }
                         // while ($row = mysqli_fetch_assoc($tasks)) {
@@ -99,7 +108,7 @@
                     }
                     ?>
                 </div>
-                <!-- <?php foreach ($tasks as $task): ?>
+                <?php foreach ($tasks as $task): ?>
                     
                     <form class="each-task" action="includes/deleteTask.php" method="post">
                         <div class="end-to-end">
@@ -121,7 +130,7 @@
                             </button>
                         </div>
                     </form>
-                <?php endforeach; ?> -->
+                <?php endforeach; ?>
             </div>
         </div>
 
