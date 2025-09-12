d23b98a973699706201ae07d8dbb8b2b06e25e3e
Dalha <dalhalawand@gmail.com>
2025-09-12 20:56:58 +0100

    commit

--- To Do List/index.php
+++ To Do List/index.php
@@ -38,24 +38,16 @@
 
         // Script to load more tasks by 2
 
-        $(document).on("click", "#btn", function(){
+        
+        $(document).ready(function(){
             var taskCount = 2;
-            taskCount = taskCount + 2;
-            console.log("Number of Tasks to be added: ", taskCount);
-            $("#tasks").load("load.tasks.php", {
-                taskNewCount: taskCount
-            
+            $("#btn").click(function(){
+                taskCount = taskCount + 2;
+                $("#tasks").load("load.tasks.php", {
+                    taskNewCount: taskCount
+                });
             });
         });
-        // $(document).ready(function(){
-        //     var taskCount = 2;
-        //     $("#btn").click(function(){
-        //         taskCount = taskCount + 2;
-        //         $("#tasks").load("load.tasks.php", {
-        //             taskNewCount: taskCount
-        //         });
-        //     });
-        // });
 
         $(document).ready(function(){
             $("#deleteBtn").click(function(){
@@ -81,9 +73,6 @@
                     <?php
                     if ($numRows > 0) {
                         foreach ($tasks as $task) {
-                            // echo "<pre>";
-                            // print_r($tasks);
-                            // echo "</pre>";
                             echo "<div class='each-task'>";
                                 echo "<div class='end-to-end'>";
                                     echo "<input data-id='" . $task["id"] . "' class='checkbox' type='checkbox'>";
@@ -108,37 +97,12 @@
                             
                             
                         }
-                        // while ($row = mysqli_fetch_assoc($tasks)) {
                             
-                        // }
                     } else {
                         echo "You have no tasks";
                     }
                     ?>
                 </div>
-                <!-- <?php foreach ($tasks as $task): ?>
-                    
-                    <form class="each-task" action="includes/deleteTask.php" method="post">
-                        <div class="end-to-end">
-                            <input class="checkbox" type="checkbox" name="checkbox">
-                            <label class="task-content"><?php echo htmlspecialchars($task['task']); ?></label>
-                        </div>
-                        <div class="end-to-end">
-                            <select class="status" name="status">
-                                <option class="status-incomplete" value="I">Incomplete</option>
-                                <option class="status-done" value="D">Done</option>
-                            </select>
-                            
-                            <input type="hidden" name="task_id" value="<?php echo $task['id']; ?>">
-                            <button class="delete-btn" type="submit" name="delete">  
-                                <svg class="trash-can-icon"  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                                </svg>
-                            </button>
-                        </div>
-                    </form>
-                <?php endforeach; ?> -->
             </div>
         </div>
 
@@ -147,10 +111,7 @@
                 <button class="export-btn" type="submit">Export All Tasks</button>
             </form>
             
-            <button id="btn">Load more tasks...</button>
-            
-            <!-- <a id="btn" class="load-more" href="">Load more tasks...</a> -->
-
+            <button id="btn" class="load-more">Load more tasks...</button>
         </div>
         
     </div>
