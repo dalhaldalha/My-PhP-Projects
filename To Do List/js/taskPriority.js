6715c6ca76e12ffd563579316b7d35bcc8a1917c
Dalha <dalhalawand@gmail.com>
2025-09-14 22:11:34 +0100

    commit

--- To Do List/js/taskPriority.js
+++ To Do List/js/taskPriority.js
@@ -5,10 +5,10 @@ $(document).on("change", ".priority", function(){
     console.log("Task ID: ", taskID);
     
     if (val === "high") {
-        $(this).css("background-color", "#ff6b6b").css("color", "white").css("border", "#ff6b6b");
+        $(this).css("background-color", "#D22B2B").css("color", "white").css("border", "#D22B2B");
     } else if (val === "medium") {
-        $(this).css("background-color", "yellow").css("color", "white");
+        $(this).css("background-color", "orange").css("color", "white").css("border", "orange");
     } else if (val === "low") {
-        $(this).css("background-color", "blue").css("color", "white");
+        $(this).css("background-color", "#B8B8FF").css("color", "white").css("border", "#B8B8FF");
     }
 });
