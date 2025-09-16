22da3629bbcddf576627f01fcadf19ea9deab1d3
Dalha <dalhalawand@gmail.com>
2025-09-16 02:44:53 +0100

    commit

--- To Do List/index.php
+++ To Do List/index.php
@@ -64,7 +64,7 @@
                                     echo "</p>";
                                 echo "</div>";
                                 echo "<div class='end-to-end'>";
-                                    echo "<svg class='starIcon' xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='yellow' viewBox='0 0 24 24' border='solid'>";
+                                    echo "<svg class='starIcon' xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='transparent' viewBox='0 0 24 24' stroke='hsl(272, 81%, 47%)'>";
                                         echo "<path d='M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z'/>";
                                     echo "</svg>";
                                     // echo "<select data-id='" . $task["id"] . "' id='priority' class='priority'>";
