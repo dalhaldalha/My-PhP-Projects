a845e64694925dbfe5eb85315aaf9f492b8672e9
Dalha <dalhalawand@gmail.com>
2025-09-17 01:16:53 +0100

    commit

--- To Do List/includes/taskPriority.php
+++ To Do List/includes/taskPriority.php
@@ -7,16 +7,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $taskID = $_POST["id"];
     $taskPriority = $_POST["priority"];
 
-    try {
-        $query = "UPDATE tasks SET priority = ? WHERE id = ?";
-        $stmt = pdo->prepare($query);
-        $stmt->execute([$taskPriority, $taskID]);
-        
-    } catch (PDOException $e){
-        echo json_encode(["error" => $e->getMessage()]);
-    }
     
+    $query = "UPDATE tasks SET priority = ? WHERE id = ?";
+    $stmt = pdo->prepare($query);
+    if ($stmt->execute([$taskPriority, $taskID])){
+        echo json_encode("Updated succesfully");
+    } else {
+        echo json_encode("Update failed!");
+    }
 
+    
 }
 
 
